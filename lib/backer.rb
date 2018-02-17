320c17858351de73b3ee9edade500c2ce3c8d7c0
Jay Bednar <ironviralmedia@gmail.com>
2018-02-17 18:59:48 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -26,7 +26,7 @@ class Backer
   end
 
    def back_project(project)
-    self.backed_projects = Project.find_or_create_by_title(title)
+    self.backed_projects << Project.find_or_create_by_title(title)
 
     # self.project.add_backer(self)
    end
