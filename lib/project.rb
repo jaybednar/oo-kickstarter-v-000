b99af7c15a9f12e3cd0247fc4837360af9e98222
Jay Bednar <ironviralmedia@gmail.com>
2018-02-17 18:00:39 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,13 +1,17 @@
 class Project
   attr_accessor :title, :backers
 
+  @@all_projects = []
+
   def initialize(title)
     @title = title
     @backers = []
   end
 
   def add_backer(backer)
-    @backers << backer 
+    @backers << backer
   end
 
+  def find_or_create(project)
+    if !@backers
 end
